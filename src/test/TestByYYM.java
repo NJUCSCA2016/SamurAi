0530d63d24dda85187d84c7e960234408f2193c1
SwordsKing <979579683@qq.com>
2016-04-02 20:33:55 +0800

    YYMTest

---
+++ src/test/TestByYYM.java
@@ -0,0 +1,17 @@
+package test;
+
+public class TestByYYM {
+	
+	public static void main(String[] args) {
+	
+		new TestByYYM().run();
+	
+	}
+	
+	public void run(){
+		
+		System.out.println("Test By YYm");
+		
+	}
+	
+}
--- src/test/TestWan.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-public class TestWan {
-	public static void main(String[] args) {
-		System.out.println("This is wan's test");
-	}
-}
