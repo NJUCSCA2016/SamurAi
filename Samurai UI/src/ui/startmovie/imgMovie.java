ab7c9f8e850bb06f5a3a78e9b2b49cf80e68cbbc
Water <598127577@qq.com>
2016-04-04 17:38:15 +0800

    开场动画已经实现了 但是好丑

--- Samurai UI/src/ui/startmovie/imgMovie.java
+++ Samurai UI/src/ui/startmovie/imgMovie.java
@@ -3,6 +3,8 @@
  */
 package ui.startmovie;
 
+import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
@@ -10,30 +12,43 @@ import javax.swing.ImageIcon;
 /**
  * @author Alone
  * Written by YYM
+ * Also by WZ
  */
-public class ImgMovie implements Runnable{
+public class ImgMovie extends Frame implements Runnable{
 	
-	public static Image[] image;
+	public Image image;
 	
 	/**
 	 * 线程式加载图片
 	 */
 
 	public void run() {
-		image = new Image[70];
 		try {
 		//TODO : 还要加载 Loading和片尾图片
 			for(int i = 1 ; i <= 70 ;i++){
 				System.out.println("Read");
 				//经测试，ImageIcon比ImageIo快很多
-				image[i-1] = new ImageIcon("Image/Start/" + i + ".png").getImage();
-			
+				image = new ImageIcon("Image/Start/" + i + ".png").getImage();
+				repaint();
+				Thread.sleep(50);
 			}
 		}
 		catch (Exception e) {
 				e.printStackTrace();
 		}
 	}
+	public ImgMovie(){
+		super("ImgMovie");
+		setSize(1250,700);
+		setVisible(true);
+		new Thread(this).start();
+	}
 	
+	public void paint(Graphics g){
+		g.drawImage(image, 0, 0, null);
+	}
+	public static void main(String[] args) {
+		ImgMovie workStart = new ImgMovie();
+	}
 	
 }
