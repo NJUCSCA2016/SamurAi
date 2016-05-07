25bcdda74026f65c2ae585f783bfb1de7d983e33
SwordsKing <979579683@qq.com>
2016-05-08 01:32:37 +0800

    改错位置了

--- AI/src/ai/PlayerSword.java
+++ AI/src/ai/PlayerSword.java
@@ -2,13 +2,40 @@ package ai;
 
 public class PlayerSword extends Player{
 	
+	/**
+	 *  假设有三个在上场 ， 你只能杀一个。那么可能杀了以后你就死了。就逃跑吧
+	 */
+	public final static int RUN_AWAY = 0;
+	/**
+	 * 可以杀一个人并且应该为安全的
+	 */
+	public final static int CAN_KILL_ONE = 1;
+	/**
+	 * 双杀。不需要考虑会不会死。死俩值了
+	 */
+	public final static int DOUBLE_KILL = 2;
+	/**
+	 * 嘿嘿
+	 */
+	public final static int TRIPLE_KILL = 3;
+	/**
+	 * 该片区域全部被占领了，并且没有人在。
+	 */
+	public final static int OCCUPY_ALL = 4;
+	/**
+	 * 没人并且部分占领了。
+	 */
+	public final static int NOBODY_PART = 5;
+	
+	
+	
 	@Override
 	public void play() {
 		// TODO Auto-generated method stub
 		
 	}
 	
-	public int 
+
 	
 	
 	
