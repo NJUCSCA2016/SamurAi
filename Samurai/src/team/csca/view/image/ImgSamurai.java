c787c76d61fe2c970b40eac8621adb7ab5b82930
Water <598127577@qq.com>
2016-05-08 21:02:29 +0800

    需要的图

--- Samurai/src/team/csca/view/image/ImgSamurai.java
+++ Samurai/src/team/csca/view/image/ImgSamurai.java
@@ -21,4 +21,28 @@ public class ImgSamurai {
 	public static final Image AI_5 = new ImageIcon("Image/AI/2AI5.png").getImage();
 	
 	public static final Image AI_6 = new ImageIcon("Image/AI/2AI6.png").getImage();
+	
+	public static final Image AI_1_LEFT = new ImageIcon("Image/AI/AI1_LEFT.png").getImage();
+	
+	public static final Image AI_2_LEFT = new ImageIcon("Image/AI/AI2_LEFT.png").getImage();
+	
+	public static final Image AI_3_LEFT = new ImageIcon("Image/AI/AI3_LEFT.png").getImage();
+	
+	public static final Image AI_4_LEFT = new ImageIcon("Image/AI/AI4_LEFT.png").getImage();
+	
+	public static final Image AI_5_LEFT = new ImageIcon("Image/AI/AI5_LEFT.png").getImage();
+	
+	public static final Image AI_6_LEFT = new ImageIcon("Image/AI/AI6_LEFT.png").getImage();
+	
+	public static final Image AI_1_RIGHT = new ImageIcon("Image/AI/AI1_RIGHT.png").getImage();
+	
+	public static final Image AI_2_RIGHT = new ImageIcon("Image/AI/AI2_RIGHT.png").getImage();
+
+	public static final Image AI_3_RIGHT = new ImageIcon("Image/AI/AI3_RIGHT.png").getImage();
+	
+	public static final Image AI_4_RIGHT = new ImageIcon("Image/AI/AI4_RIGHT.png").getImage();
+	
+	public static final Image AI_5_RIGHT = new ImageIcon("Image/AI/AI5_RIGHT.png").getImage();
+	
+	public static final Image AI_6_RIGHT = new ImageIcon("Image/AI/AI6_RIGHT.png").getImage();
 }
