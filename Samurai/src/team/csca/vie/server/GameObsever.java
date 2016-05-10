bf9551ec8b43675c8154ec6a25e85b092122153e
SwordsKing <979579683@qq.com>
2016-05-10 10:46:38 +0800

    本地服务调用

---
+++ Samurai/src/team/csca/vie/server/GameObsever.java
@@ -0,0 +1,42 @@
+package team.csca.vie.server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface GameObsever extends Remote{
+	
+	/**
+	 * 接受传统模式用户传来的动作
+	 * 
+	 * @param action
+	 * @throws RemoteException
+	 */
+	public void acceptActionTro(String action) throws RemoteException;
+	/**
+	 * 接受道具模式下用户传来的动作。
+	 * 
+	 * @param action 动作。使用split("\\s")即可进行分割
+	 * @throws RemoteException
+	 */
+	public void acceptActionProp(String action) throws RemoteException;
+	/**
+	 * 初始化信息反馈给每个Player
+	 * 
+	 * @return
+	 * @throws RemoteException
+	 */
+	public String initGame() throws RemoteException;
+	/**
+	 * 将游戏信息反馈给指定的AI
+	 * 
+	 * @return 所返回的信息。
+	 * 
+	 * 反馈格式可参考AI中的GameInfo。
+	 * 
+	 * @throws RemoteException
+	 */
+	public String feedBack() throws RemoteException;
+	
+	
+	
+}
