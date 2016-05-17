82febbe378e3fb7b8b4d5d41420edd0fb389a4a1
Water <598127577@qq.com>
2016-05-17 20:54:20 +0800

    加图

--- Samurai/src/team/csca/view/image/ImgGameOver.java
+++ Samurai/src/team/csca/view/image/ImgGameOver.java
@@ -9,4 +9,12 @@ public class ImgGameOver {
 	 * 排行榜
 	 */
 	public static Image RANKING_LIST = new ImageIcon("Image/Gamepanel/rankingList.png").getImage();
+	/**
+	 * 胜利
+	 */
+	public static Image YOU_WIN = new ImageIcon("Image/Gamepanel/win.png").getImage();
+	/**
+	 * 失败
+	 */
+	public static Image YOU_LOSE = new ImageIcon("Image/Gamepanel/win.png").getImage();
 }
