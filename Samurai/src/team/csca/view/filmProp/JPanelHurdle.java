09241ee1409455af77ca30e8dc1c5e3176e7859b
Water <598127577@qq.com>
2016-05-29 13:10:22 +0800

    Revert "/"
    This reverts commit a731eaeae9116da0e3446567f16c5fc5e51e01db.

--- Samurai/src/team/csca/view/filmProp/JPanelHurdle.java
+++ Samurai/src/team/csca/view/filmProp/JPanelHurdle.java
@@ -27,6 +27,7 @@ public class JPanelHurdle extends JPanel{
 	public JPanelHurdle() {
 		this.setLayout(null);
 		label = new JLabel();
+		label.setSize(407,299);
 		background = new LayerBackground(0, 0, 1250, 700, ImgMap.MAPS[JPanelHurdle.curHurdle]);
 		this.add(new JButtonForHurdleChoose(curHurdle, this));
 		this.add(new JButtonBackToMain(this));
@@ -50,33 +51,42 @@ public class JPanelHurdle extends JPanel{
 	}
 	
 	public void playScroll(){
-		if(label == null){
-			this.label = new JLabel();
-			this.label.setOpaque(true);
-		}
 		this.add(label);
-		while(mouseIn && labelCount < 7){
-			labelCount ++ ;
-			this.label.setIcon(getScrollIcon(labelCount));
+//		while(mouseIn && labelCount < 7){
+//			labelCount ++ ;
+//			try {
+//				Thread.sleep(150);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			this.label.setIcon(getScrollIcon(7));
 			repaint();
-		}
+//		}
 	}
 	
 	public void closeScroll(){
 		
-		while((!mouseIn) && labelCount > 0){
-			labelCount --;
-			this.label.setIcon(getScrollIcon(labelCount));
-			repaint();
-		}
+//		while((!mouseIn) && labelCount > 0){
+//			labelCount --;
+//			try {
+//				Thread.sleep(100);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			this.label.setIcon(getScrollIcon(labelCount));
+//			repaint();
+//		}
 		
-		if(labelCount == 0){
+//		if(labelCount == 0){
 			this.remove(this.label);
-		}
+//		}
 		repaint();
 	}
 	
 	private Icon getScrollIcon(int picCount){
+		System.out.println("Get");
 		return new ImageIcon("Image/GamePanel/situation/stage/stage" + (curHurdle+1)+ "_" + picCount + ".png");
 	}
 	
