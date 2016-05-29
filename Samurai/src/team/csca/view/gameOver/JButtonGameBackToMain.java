ac25d64eaf2a81566d94f97688d63ae3b73c0215
Water <598127577@qq.com>
2016-05-29 11:38:06 +0800

    修改

--- Samurai/src/team/csca/view/gameOver/JButtonGameBackToMain.java
+++ Samurai/src/team/csca/view/gameOver/JButtonGameBackToMain.java
@@ -8,32 +8,6 @@ import team.csca.view.image.ImgGameOver;
 import team.csca.view.startgame.JPanelStartGame;
 
 public class JButtonGameBackToMain extends DynamicButton{
-//	public JButtonGameBackToMain(){
-//		super(1200, 50, 50, 50, ImgGameOver.CROSS, ImgGameOver.CROSS, ImgGameOver.CROSS);
-//	}
-//	private JPanelGameWin gameWin;
-//	
-//	private JPanelGameDraw gameDraw;
-//	
-//	private JPanelGameLose gameLose;
-//
-//	public JButtonGameBackToMain(JPanelGameWin fatherPanel){
-//		// TODO:调整位置
-//		super(1150, 0, 71, 71, ImgGameOver.CROSS, ImgGameOver.CROSS, ImgGameOver.CROSS);
-//		this.gameWin = fatherPanel;
-//	}
-//	
-//	public JButtonGameBackToMain(JPanelGameLose fatherPanel){
-//		// TODO:调整位置
-//		super(1150, 0, 71, 71, ImgGameOver.CROSS, ImgGameOver.CROSS, ImgGameOver.CROSS);
-//		this.gameLose = fatherPanel;
-//	}
-//	
-//	public JButtonGameBackToMain(JPanelGameDraw fatherPanel){
-//		// TODO:调整位置
-//		super(1150, 0, 71, 71, ImgGameOver.CROSS, ImgGameOver.CROSS, ImgGameOver.CROSS);
-//		this.gameDraw = fatherPanel;
-//	}
 	public JButtonGameBackToMain() {
 		super(1150, 0, 71, 71, ImgGameOver.CROSS, ImgGameOver.CROSS, ImgGameOver.CROSS);
 	}
